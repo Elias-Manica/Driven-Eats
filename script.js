01e3d58b0686ed3fb5a0817104c1fa231978c3b8
Elias <eliasmanica@poli.ufrj.br>
2022-06-11 20:46:25 -0300

    botão de pedido só atualiza quando os 3 itens sao escolhidos

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
+let botaoClicado;
+let botaoClicadoBebi;
+let botaoClicadoSobre;
+let fecharPedido;
+let selecionarComida;
+let selecionarBebida;
+let selecionarSobremesa;
+let icone;
+
 function botaoSelecionado(elemento) {
-  const botaoClicado = document.querySelector(".principal .escondido");
+  selecionarComida = elemento.innerHTML;
+
+  icone = document.querySelector(".principal .icone");
+
+  botaoClicado = document.querySelector(".principal .escondido");
   if (botaoClicado !== null) {
     botaoClicado.classList.remove("escondido");
+    icone.classList.add("oculto");
   }
   elemento.classList.add("escondido");
+  icone.classList.remove("oculto");
+  selecionarPedido();
 }
 
 function botaoSelecionadoBebida(elemento) {
-  const botaoClicadoBebi = document.querySelector(".bebida .escondido ");
+  selecionarBebida = elemento.innerHTML;
+
+  icone = document.querySelector(".bebida .icone");
+
+  botaoClicadoBebi = document.querySelector(".bebida .escondido ");
   if (botaoClicadoBebi !== null) {
     botaoClicadoBebi.classList.remove("escondido");
   }
   elemento.classList.add("escondido");
+  icone.classList.remove("oculto");
+  selecionarPedido();
 }
 
 function botaoSelecionadoSobremesa(elemento) {
-  const botaoClicadoSobre = document.querySelector(".sobremesa .escondido ");
+  selecionarSobremesa = elemento.innerHTML;
+
+  icone = document.querySelector(".sobremesa .icone");
+
+  botaoClicadoSobre = document.querySelector(".sobremesa .escondido ");
   if (botaoClicadoSobre !== null) {
     botaoClicadoSobre.classList.remove("escondido");
   }
   elemento.classList.add("escondido");
+  icone.classList.remove("oculto");
+  selecionarPedido();
+}
+
+function selecionarPedido() {
+  if (selecionarComida && selecionarBebida && selecionarSobremesa) {
+    fecharPedido = document.querySelector(".fecharPedido");
+    fecharPedido.classList.add("fundoVerde");
+    let frase = document.querySelector(".fraseBotao");
+    frase.innerHTML = "Fechar pedido";
+  }
 }

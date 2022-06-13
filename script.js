df3172b90dd6c643b3723117fd956c8ec246b8bc
Elias <eliasmanica@poli.ufrj.br>
2022-06-12 21:28:25 -0300

    aplicativo mandando a mensagem para o cliente

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ let fecharPedido;
 let selecionarComida;
 let selecionarBebida;
 let selecionarSobremesa;
-let icone;
+let msg;
+let message;
+let nomeComida;
+let precoComida;
+let nomeBebida;
+let precoBebida;
+let nomeSobremesa;
+let precoSobremesa;
 
 function botaoSelecionado(elemento) {
   selecionarComida = elemento.innerHTML;
+  nomeComida = elemento.firstElementChild.textContent;
+  precoComida = elemento.lastElementChild.textContent;
 
   botaoClicado = document.querySelector(".principal .escondido");
   if (botaoClicado !== null) {
@@ -23,6 +32,8 @@ function botaoSelecionado(elemento) {
 
 function botaoSelecionadoBebida(elemento) {
   selecionarBebida = elemento.innerHTML;
+  nomeBebida = elemento.firstElementChild.textContent;
+  precoBebida = elemento.lastElementChild.textContent;
 
   botaoClicadoBebi = document.querySelector(".bebida .escondido ");
   if (botaoClicadoBebi !== null) {
@@ -38,6 +49,8 @@ function botaoSelecionadoBebida(elemento) {
 
 function botaoSelecionadoSobremesa(elemento) {
   selecionarSobremesa = elemento.innerHTML;
+  nomeSobremesa = elemento.firstElementChild.textContent;
+  precoSobremesa = elemento.lastElementChild.textContent;
 
   icone = document.querySelector(".sobremesa .icone");
 
@@ -60,4 +73,39 @@ function selecionarPedido() {
     let frase = document.querySelector(".fraseBotao");
     frase.innerHTML = "Fechar pedido";
   }
+}
+
+function encaminharPedido(elemento) {
+  precoComida =
+    precoComida[precoComida.length - 5] +
+    precoComida[precoComida.length - 4] +
+    "." +
+    precoComida[precoComida.length - 2] +
+    precoComida[precoComida.length - 1];
+
+  precoBebida =
+    precoBebida[precoBebida.length - 5] +
+    precoBebida[precoBebida.length - 4] +
+    "." +
+    precoBebida[precoBebida.length - 2] +
+    precoBebida[precoBebida.length - 1];
+
+  precoSobremesa =
+    precoSobremesa[precoSobremesa.length - 5] +
+    precoSobremesa[precoSobremesa.length - 4] +
+    "." +
+    precoSobremesa[precoSobremesa.length - 2] +
+    precoSobremesa[precoSobremesa.length - 1];
+
+  let valorTotal =
+    Number(precoComida) + Number(precoBebida) + Number(precoSobremesa);
+
+  msg = `Olá, gostaria de fazer o pedido:
+  - Prato: ${nomeComida}
+  - Bebida: ${nomeBebida}
+  - Sobremesa: ${nomeSobremesa}
+Total: R$ ${valorTotal.toFixed(2)}`;
+
+  message = `https://wa.me/5521980279273?text=${encodeURIComponent(msg)}`;
+  open(message);
 }
